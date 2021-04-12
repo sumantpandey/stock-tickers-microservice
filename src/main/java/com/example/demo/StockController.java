9efb21fb02fa8bd79d057791852579e14b07aeda
sumant <you@example.com>
2021-04-12 08:05:18 +0530

    Initial Commit: changes in method to get all stocks for a given list of tickers

--- src/main/java/com/example/demo/StockController.java
+++ src/main/java/com/example/demo/StockController.java
@@ -5,14 +5,13 @@ import com.example.demo.model.StockWrapper;
 import com.example.demo.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -21,7 +20,7 @@ public class StockController {
     @Autowired
     private StockService stockservice;
 
-    @GetMapping("/{ticker}")
+    @GetMapping("/stocks/{ticker}")
     public ResponseEntity<StockWrapper> getStock(@PathVariable("ticker") String ticker) {
 
         StockWrapper stock = stockservice.findStock(ticker);
@@ -33,16 +32,17 @@ public class StockController {
 
     }
 
-    @GetMapping("/{ticker}")
-    public ResponseEntity<List<StockWrapper>> getStock(@RequestParam(value="tickers") List<String> tickers) {
+    @PostMapping("/stocks/findAll")
+    @ResponseBody
+    public ResponseEntity<List<StockWrapper>> getStocks(@RequestBody List<String> tickers) {
 
-        List<StockWrapper> stock = stockservice.findStocks(tickers);
+        List<StockWrapper> stocks = stockservice.findStocks(tickers);
 
-        return new ResponseEntity<List<StockWrapper>>(stock, HttpStatus.OK);
+        return new ResponseEntity<List<StockWrapper>>(stocks, HttpStatus.OK);
 
     }
 
-    @GetMapping("/{ticker}")
+    @GetMapping("stocks/price/{ticker}")
     public ResponseEntity<BigDecimal> getStockPrice(@PathVariable("ticker") String ticker) throws IOException {
 
         StockWrapper sw = stockservice.findStock(ticker);
